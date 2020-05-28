7025d090a446f61b1e41ef08ef798a9e4592fd0c
Mats Sjöberg <mats.sjoberg@csc.fi>
2020-05-28 17:26:10 +0300

    Add argparse and other cleanups

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 rm -rf ~/.local/lib/python3.*
 rm ~/ml-webinar/examples/slurm*out
+cd ~/ml-webinar
+git checkout -- examples/run.sh
+git status
+env | grep SBATCH_
--- examples/dogs_vs_cats.py
+++ examples/dogs_vs_cats.py
@@ -1,63 +1,68 @@
-import os, datetime
-import random
+import argparse
+import os
 import pathlib
-
+import random
 import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import (Dense, Activation, Dropout, Conv2D,
-                                    Flatten, MaxPooling2D, InputLayer)
-from tensorflow.keras.preprocessing.image import (ImageDataGenerator, 
-                                                  array_to_img, 
-                                                  img_to_array, load_img)
-from tensorflow.keras import applications, optimizers
-
-from tensorflow.keras.callbacks import TensorBoard
-
-import numpy as np
+from tensorflow.keras.layers import (Dense, Dropout, Conv2D, MaxPooling2D,
+                                     Flatten)
 
 print('Using Tensorflow version:', tf.__version__)
 
-EPOCHS=20
-CPUS=10
-BATCH_SIZE=32
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, default=20)
+parser.add_argument('--cpus', type=int, default=10)
+parser.add_argument('--datadir', type=str, default=None)
+args = parser.parse_args()
+
+BATCH_SIZE = 32
 INPUT_IMAGE_SIZE = [160, 160, 3]
 
-if 'DATADIR' in os.environ:
-    DATADIR = os.environ['DATADIR']
-else:
+if args.datadir is None:
     DATADIR = "/scratch/dac/data/"
+else:
+    DATADIR = args.datadir
 
 print('Using DATADIR', DATADIR)
 datapath = os.path.join(DATADIR, "dogs-vs-cats/train-2000/")
 
+print('Using {} CPUs'.format(args.cpus))
+
 nimages = dict()
 nimages['train'] = 2000
 nimages['validation'] = 1000
+
 
 def get_paths(dataset):
     data_root = pathlib.Path(datapath+dataset)
     image_paths = list(data_root.glob('*/*'))
     image_paths = [str(path) for path in image_paths]
     image_count = len(image_paths)
-    assert image_count == nimages[dataset], "Found {} images, expected {}".format(image_count, nimages[dataset])
+    assert image_count == nimages[dataset], \
+        "Found {} images, expected {}".format(image_count, nimages[dataset])
     return image_paths
+
 
 image_paths = dict()
 image_paths['train'] = get_paths('train')
 image_paths['validation'] = get_paths('validation')
 
-label_names = sorted(item.name for item in pathlib.Path(datapath+'train').glob('*/')
+label_names = sorted(item.name for item in
+                     pathlib.Path(datapath+'train').glob('*/')
                      if item.is_dir())
-label_to_index = dict((name, index) for index,name in enumerate(label_names))
+label_to_index = dict((name, index) for index, name in enumerate(label_names))
+
 
 def get_labels(dataset):
     return [label_to_index[pathlib.Path(path).parent.name]
             for path in image_paths[dataset]]
-    
+
+
 image_labels = dict()
 image_labels['train'] = get_labels('train')
 image_labels['validation'] = get_labels('validation')
+
 
 def preprocess_image(image, augment):
     image = tf.image.decode_jpeg(image, channels=3)
@@ -71,25 +76,28 @@ def preprocess_image(image, augment):
     image /= 255.0  # normalize to [0,1] range
     return image
 
+
 def load_and_augment_image(path, label):
     image = tf.io.read_file(path)
     return preprocess_image(image, True), label
 
+
 def load_and_not_augment_image(path, label):
     image = tf.io.read_file(path)
     return preprocess_image(image, False), label
+
 
 train_dataset = tf.data.Dataset.from_tensor_slices((image_paths['train'],
                                                     image_labels['train']))
 validation_dataset = tf.data.Dataset.from_tensor_slices((image_paths['validation'],
                                                          image_labels['validation']))
 
-train_dataset = train_dataset.map(load_and_augment_image, num_parallel_calls=CPUS)
+train_dataset = train_dataset.map(load_and_augment_image, num_parallel_calls=args.cpus)
 train_dataset = train_dataset.shuffle(2000).batch(BATCH_SIZE, drop_remainder=True)
 train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
 validation_dataset = validation_dataset.map(load_and_not_augment_image,
-                                            num_parallel_calls=CPUS)
+                                            num_parallel_calls=args.cpus)
 validation_dataset = validation_dataset.batch(BATCH_SIZE, drop_remainder=True)
 validation_dataset = validation_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
@@ -113,9 +121,9 @@ model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-history = model.fit(train_dataset, epochs=EPOCHS,
-                    validation_data=validation_dataset,
-                    verbose=2)
+model.fit(train_dataset, epochs=args.epochs,
+          validation_data=validation_dataset,
+          verbose=2)
 
 # fname = "dvc-cnn-simple.h5"
 # print('Saving model to', fname)
