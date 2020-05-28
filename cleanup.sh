54cec011f46ea083752b004fee3851415a15bd80
Mats Sjöberg <mats.sjoberg@csc.fi>
2020-05-28 17:42:12 +0300

    Also reset examples/dogs_vs_cats.py in cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,6 @@
 rm -rf ~/.local/lib/python3.*
 rm ~/ml-webinar/examples/slurm*out
 cd ~/ml-webinar
-git checkout -- examples/run.sh
+git checkout -- examples/run.sh examples/dogs_vs_cats.py
 git status
 env | grep SBATCH_
